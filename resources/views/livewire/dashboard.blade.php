0d1981019d36041348b9dc066a56a59eee727fb9
Frenz R <125328585+ChimaRyder@users.noreply.github.com>
2025-07-15 19:31:28 +0800

    Dashboard

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <div class="drawer drawer-open">
+        <input id="sidebar" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center justify-center">
+
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+            <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4 *:text-lg gap-4">
+
+                <li><button class="btn btn-lg bg-amber-700 text-white">Write A Post</button></li>
+                <li><a href=""><x-fas-user class="w-6 h-6"/>Profile</a></li>
+                <li><a href=""><x-fas-note-sticky class="w-6 h-6"/>My Posts</a></li>
+                <li><a href=""><x-fas-gear class="w-6 h-6"/>Settings</a></li>
+
+            </ul>
+        </div>
+    </div>
+</div>
